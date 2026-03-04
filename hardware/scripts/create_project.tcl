5ca6a94499db386a315de67a0344d830d47846b1
ignaciogarciae <I.GarciaEzquerro@tudelft.nl>
2026-03-04 11:07:28 +0100

    fixed missing changes

--- hardware/scripts/create_project.tcl
+++ hardware/scripts/create_project.tcl
@@ -7,13 +7,6 @@ add_files -norecurse ./src/design/riscy/include/
 add_files -norecurse ./src/design/zynq_system/
 add_files -norecurse ./src/simulation/
 
-# Make sure the include files/headers are recognized as such.
-# If they had properly been named with the extension .vh we wouldn't need this.
-set_property file_type {Verilog Header} [get_files apu_core_package.v]
-set_property file_type {Verilog Header} [get_files apu_macros.v]
-set_property file_type {Verilog Header} [get_files riscv_config.v]
-set_property file_type {Verilog Header} [get_files riscv_defines.v]
-
 update_compile_order -fileset sources_1
 
 # Generate fpga system bd:
--- hardware/scripts/create_project_ooc_synth.tcl
+++ hardware/scripts/create_project_ooc_synth.tcl
@@ -5,13 +5,6 @@ create_project ooc_riscy ./vivado/ooc_riscy -part xc7z020clg400-1 -force
 add_files -norecurse ./src/design/riscy/
 add_files -norecurse ./src/design/riscy/include/
 
-# Make sure the include files/headers are recognized as such.
-# If they had properly been named with the extension .vh we wouldn't need this.
-set_property file_type {Verilog Header} [get_files apu_core_package.v]
-set_property file_type {Verilog Header} [get_files apu_macros.v]
-set_property file_type {Verilog Header} [get_files riscv_config.v]
-set_property file_type {Verilog Header} [get_files riscv_defines.v]
-
 update_compile_order -fileset sources_1
 
 set_property -name {STEPS.SYNTH_DESIGN.ARGS.MORE OPTIONS} -value -mode_out_of_context -objects [get_runs synth_1]
