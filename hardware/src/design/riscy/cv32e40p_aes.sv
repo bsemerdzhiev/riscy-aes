fcb1091fa5ff3002a93ba202e839d32cc70417fe
isidorajovanovic <isidorajova@gmail.com>
2026-05-12 16:10:19 +0200

    dsmi done? but idk what to do with dsi

--- hardware/src/design/riscy/cv32e40p_aes.sv
+++ hardware/src/design/riscy/cv32e40p_aes.sv
@@ -2,7 +2,7 @@
 // Engineer:       Borislav Semerdzhiev - bsemerdzhiev@tudelft.nl             //
 //                                                                            //
 // Additional contributions by:                                               //
-//                                                                            //
+// Isidora Jovanovic - Decryption                                             //
 //                                                                            //
 //                                                                            //
 // Design Name:    AES                                                        //
@@ -50,41 +50,66 @@ endmodule
 
 module mix_columns#()
 (
-//inputs
+  //inputs
   input logic [7: 0] col,
   input logic [1:0]  bs,
 
+  // 0 - encryption / 1 - decryption
   input logic direction,
 
-//outputs
+  //outputs
   output logic [31:0] result
 );
 
-logic [7:0] results[0:1];
+// encryption
+logic [7:0] results_enc[0:1];
 
-gf_mult u_col_one   (.a(8'h02), .b(col), .result(results[0]));
-gf_mult u_col_two   (.a(8'h03), .b(col), .result(results[1]));
+gf_mult u_col_one   (.a(8'h02), .b(col), .result(results_enc[0]));
+gf_mult u_col_two   (.a(8'h03), .b(col), .result(results_enc[1]));
+
+// decryption
+logic [7:0] results_dec[0:3];
+
+gf_mult u_col_one_dec (.a(8'h0e), .b(col), .result(results_dec[0]));
+gf_mult u_col_two_dec (.a(8'h0b), .b(col), .result(results_dec[1]));
+gf_mult u_col_three_dec (.a(8'h0d), .b(col), .result(results_dec[2]));
+gf_mult u_col_four_dec (.a(8'h09), .b(col), .result(results_dec[3]));
+
 
 always_comb begin
   result = 32'h0;
 
   case(bs)
-    //TODO: Check the direction forward or backward
-    
     2'd0: begin
-      result = {results[0], col, col, results[1]};
+      if (direction == '0) begin
+        result = {results_enc[0], col, col, results_enc[1]};
+      end else begin
+        result = {results_dec[0], results_dec[1], results_dec[2], results_dec[3]};
+      end
     end
 
     2'd1: begin
-      result = {results[1], results[0], col, col};
+      if (direction == '0) begin
+        result = {results_enc[1], results_enc[0], col, col};
+      end else begin
+        result = {results_dec[3], results_dec[0], results_dec[1], results_dec[2]};
+      end
     end
 
     2'd2: begin
-      result = {col, results[1], results[0], col};
+      if (direction == '0) begin
+        result = {col, results_enc[1], results_enc[0], col};
+      end else begin
+        result = {results_dec[2], results_dec[3], results_dec[0], results_dec[1]};
+      end
     end
 
     2'd3: begin
-      result = {col, col, results[1], results[0]};
+      if (direction == '0) begin
+        result = {col, col, results_enc[1], results_enc[0]};
+      end else begin
+        result = {results_dec[1], results_dec[2], results_dec[3], results_dec[0]};
+      end
     end
   endcase
 end
@@ -96,67 +121,83 @@ module cv32e40p_aes
   import aes_pkg::*;
 #()
 (
-//inputs
-    input logic [1:0]         bs_i,
-    input logic [31:0]        matrix_input_i,
-    input logic [31:0]        key_i,
+  //inputs
+  input logic [1:0]         bs_i,
+  input logic [31:0]        matrix_input_i,
+  input logic [31:0]        key_i,
 
-    input logic [2:0]         chosen_op_i,
+  // 00 - esmi, 01 - esi, 10 - dsmi, 11 - dsi
+  input logic [1:0]         chosen_op_i,
 
-//outputs
-    output logic [31:0]       mix_columns_o
+  //outputs
+  output logic [31:0]       mix_columns_o
 );
 
 logic [5:0] shamt;
 logic [7:0] sbox_input;
 logic [7:0] sbox_output;
+logic direction = chosen_op_i[0];
 
 logic [7:0] mix_columns_col_i;
 
-mix_columns mix_columns_i(.col(mix_columns_col_i), .bs(bs_i), .result(mix_columns_o));
-
-////////////////////////////////////////////////////////////////////
-//           ______  _____ ____ ___  ______  _____ __  __ _____   //
-//     /\   |  ____|/ ____|___ \__ \|  ____|/ ____|  \/  |_   _|  //
-//    /  \  | |__  | (___   __) | ) | |__  | (___ | \  / | | |    //
-//   / /\ \ |  __|  \___ \ |__ < / /|  __|  \___ \| |\/| | | |    //
-//  / ____ \| |____ ____) |___) / /_| |____ ____) | |  | |_| |_   //
-// /_/    \_\______|_____/|____/____|______|_____/|_|  |_|_____|  //
-////////////////////////////////////////////////////////////////////
+mix_columns mix_columns_i(.col(mix_columns_col_i), .bs(bs_i), .direction(direction), .result(mix_columns_o));
 
 always_comb begin
   shamt = {1'b0, bs_i, 3'b0};
   sbox_input = matrix_input_i[shamt +: 8];
-  sbox_output = AES_SBOX[sbox_input];
 
-  mix_columns_col_i = key_i ^ sbox_output;
+  case(chosen_op_i)
+
+    ////////////////////////////////////////////////////////////////////
+    //           ______  _____ ____ ___  ______  _____ __  __ _____   //
+    //     /\   |  ____|/ ____|___ \__ \|  ____|/ ____|  \/  |_   _|  //
+    //    /  \  | |__  | (___   __) | ) | |__  | (___ | \  / | | |    //
+    //   / /\ \ |  __|  \___ \ |__ < / /|  __|  \___ \| |\/| | | |    //
+    //  / ____ \| |____ ____) |___) / /_| |____ ____) | |  | |_| |_   //
+    // /_/    \_\______|_____/|____/____|______|_____/|_|  |_|_____|  //
+    ////////////////////////////////////////////////////////////////////
+    2'b00: begin
+      sbox_output = AES_SBOX[sbox_input];
+      mix_columns_col_i = key_i ^ sbox_output;
+    end
+
+    /////////////////////////////////////////////////////////////
+    //           ______  _____ ____ ___  ______  _____ __  __  //
+    //     /\   |  ____|/ ____|___ \__ \|  ____|/ ____|_   _|  //
+    //    /  \  | |__  | (___   __) | ) | |__  | (___   | |    //
+    //   / /\ \ |  __|  \___ \ |__ < / /|  __|  \___ \  | |    //
+    //  / ____ \| |____ ____) |___) / /_| |____ ____) |_| |_   //
+    // /_/    \_\______|_____/|____/____|______|_____/|_____|  //
+    /////////////////////////////////////////////////////////////
+    // TODO: add logic for esi
+
+
+    ////////////////////////////////////////////////////////////////////
+    //           ______  _____ ____ ___  _____   _____ __  __ _____   //
+    //     /\   |  ____|/ ____|___ \__ \|  __ \ / ____|  \/  |_   _|  //
+    //    /  \  | |__  | (___   __) | ) | |  | | (___ | \  / | | |    //
+    //   / /\ \ |  __|  \___ \ |__ < / /| |  | |\___ \| |\/| | | |    //
+    //  / ____ \| |____ ____) |___) / /_| |__| |____) | |  | |_| |_   //
+    // /_/    \_\______|_____/|____/____|_____/|_____/|_|  |_|_____|  //
+    ////////////////////////////////////////////////////////////////////
+    2'b10: begin
+      sbox_output = AES_INV_SBOX[sbox_input];
+      mix_columns_col_i = key_i ^ sbox_output;
+    end
+
+    /////////////////////////////////////////////////////////////
+    //           ______  _____ ____ ___  _____   _____ _____   //
+    //     /\   |  ____|/ ____|___ \__ \|  __ \ / ____|_   _|  //
+    //    /  \  | |__  | (___   __) | ) | |  | | (___   | |    //
+    //   / /\ \ |  __|  \___ \ |__ < / /| |  | |\___ \  | |    //
+    //  / ____ \| |____ ____) |___) / /_| |__| |____) |_| |_   //
+    // /_/    \_\______|_____/|____/____|_____/|_____/|_____|  //
+    /////////////////////////////////////////////////////////////
+    2'b11: begin
+      sbox_output = AES_INV_SBOX[sbox_input];
+      // im a bit lost on how to continue
+    end
+   endcase
 end
-
-/////////////////////////////////////////////////////////////
-//           ______  _____ ____ ___  ______  _____ __  __  //
-//     /\   |  ____|/ ____|___ \__ \|  ____|/ ____|_   _|  //
-//    /  \  | |__  | (___   __) | ) | |__  | (___   | |    //
-//   / /\ \ |  __|  \___ \ |__ < / /|  __|  \___ \  | |    //
-//  / ____ \| |____ ____) |___) / /_| |____ ____) |_| |_   //
-// /_/    \_\______|_____/|____/____|______|_____/|_____|  //
-/////////////////////////////////////////////////////////////
-
-////////////////////////////////////////////////////////////////////
-//           ______  _____ ____ ___  _____   _____ __  __ _____   //
-//     /\   |  ____|/ ____|___ \__ \|  __ \ / ____|  \/  |_   _|  //
-//    /  \  | |__  | (___   __) | ) | |  | | (___ | \  / | | |    //
-//   / /\ \ |  __|  \___ \ |__ < / /| |  | |\___ \| |\/| | | |    //
-//  / ____ \| |____ ____) |___) / /_| |__| |____) | |  | |_| |_   //
-// /_/    \_\______|_____/|____/____|_____/|_____/|_|  |_|_____|  //
-////////////////////////////////////////////////////////////////////
-
-/////////////////////////////////////////////////////////////
-//           ______  _____ ____ ___  _____   _____ _____   //
-//     /\   |  ____|/ ____|___ \__ \|  __ \ / ____|_   _|  //
-//    /  \  | |__  | (___   __) | ) | |  | | (___   | |    //
-//   / /\ \ |  __|  \___ \ |__ < / /| |  | |\___ \  | |    //
-//  / ____ \| |____ ____) |___) / /_| |__| |____) |_| |_   //
-// /_/    \_\______|_____/|____/____|_____/|_____/|_____|  //
-/////////////////////////////////////////////////////////////
 
 endmodule
