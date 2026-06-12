a37377b8df5d25664d2ab7c3bdd4067b5a052a8c
Borislav Semerdzhiev <borislavsemerdzhiev.02@gmail.com>
2026-06-12 13:48:36 +0200

    adds decrypt in software only

--- hardware/move_files.sh
+++ hardware/move_files.sh
@@ -1,1 +1,1 @@
-scp -P 25551 ./vivado/riscy/riscy.runs/impl_1/riscv_wrapper.bit xilinx@5.tcp.eu.ngrok.io:/home/xilinx/jupyter_notebooks/riscy/overlays/base_riscy.bit
+scp ./vivado/riscy/riscy.runs/impl_1/riscv_wrapper.bit xilinx@192.168.178.211:/home/xilinx/jupyter_notebooks/riscy/overlays/base_riscy.bit
